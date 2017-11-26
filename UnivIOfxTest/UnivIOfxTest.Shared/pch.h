4a6124793018a333b3f9893ea831e023cb0c6d41
carlsound <johncarlson@johns-mbp.attlocal.net>
2017-11-26 13:37:18 -0600

    Added the Visual Studio project
    A Universal Windows app that uses the AudioGraph effects.

---
+++ UnivIOfxTest/UnivIOfxTest.Shared/pch.h
@@ -0,0 +1,18 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include "App.xaml.h"
+//
+#include "Common.h"
+//#include "DeviceState.h"
+//#include "WASAPICapture.h"
+//#include "WASAPIRenderer.h"
+
+//using namespace WASAPIAudio;
---
+++ UnivIOfxTest/UnivIOfxTest.WindowsPhone/MainPage.xaml.h
@@ -0,0 +1,44 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "WASAPICapture.h"
+//#include "Constants.h"
+#include <wrl.h>
+
+#include "pch.h"
+#include "MainPage.g.h"
+
+#define OSC_START_X  100
+#define OSC_START_Y  100
+#define OSC_X_LENGTH 700
+#define OSC_TOTAL_HEIGHT 200
+
+namespace UnivIOfxTest
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+		//
+	private:
+		//WASAPICapture *wc;
+		ComPtr<WASAPICapture> m_spCapture;
+
+		//
+		// TODO: add XAudio2 MasteringVoice ivar
+		//
+		// TODO: add XAudio2 SourceVoice ivar [collection (?)]
+		//
+		//SDKSample::WASAPIAudio::WASAPIRenderer wr;
+	};
+}
